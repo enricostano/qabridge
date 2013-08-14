07df67a63db0ecd49e7468d29c78cfa08ca1bd59
enricostano <ocirne.onats@gmail.com>
2013-08-14 15:04:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qabridge::Application.config.secret_key_base = '15fe87d8fff0434571f412edaff354b561d2e51184959b5649ac66efa01f6ce1c367858196f0b59fcf46ab3abf6098c315521ee27eb5c49099aff7a1568000a1'
